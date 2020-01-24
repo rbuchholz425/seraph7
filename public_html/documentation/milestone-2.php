2a1bf542e583eedc99e07f13b753264b9d0ffc3a
Ryan Buchholz <rbuchholz425@gmail.com>
2020-01-24 16:03:23 -0700

    Started Milestone-02 template.

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Seraph 7</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<h1>Content Strategy</h1>
+
+<p>My site will be one page, and will consist of the following sections:</p>
+
+<h3>Welcome:</h3>
+
+<p>
+	TEXT HERE.
+</p>
+
+<h3>About Seraph 7:</h3>
+
+<p>
+	TEXT HERE.
+</p>
+
+<h3>Discord Drop Down:</h3>
+
+<p>
+	TEXT HERE.
+</p>
+
+<h3>Contact:</h3>
+
+<p>
+	TEXT HERE.
+</p>
+
+<br>
+
+<h1>Wireframes:</h1>
+
+<img src=""alt="">
+
+</body>
+</html>
