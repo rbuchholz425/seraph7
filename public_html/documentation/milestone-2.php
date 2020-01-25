71568c1c4069d86256e851d7acad2f3663c9cc9f
Ryan Buchholz <rbuchholz425@gmail.com>
2020-01-25 08:52:34 -0700

    Finished writing initial content strategy.

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -15,32 +15,36 @@
 <h3>Welcome:</h3>
 
 <p>
-	TEXT HERE.
+	This will be a one-line statement welcoming the user to the site with a logo or design next to it and a full text logo above.
 </p>
 
 <h3>About Seraph 7:</h3>
 
 <p>
-	TEXT HERE.
+	This section will a quick short story about jumping into a new game.
+	The feelings about how it can suck to play games without friends and how this site can solve that.
+	I will talk about how easy it is to find new friends with this site.
+	And I will ask the user to jump in and find some new friends and communities!
 </p>
 
 <h3>Discord Drop Down:</h3>
 
 <p>
-	TEXT HERE.
+	Here will be a dropdown bar that links to several different discord servers on different popular games.
 </p>
 
 <h3>Contact:</h3>
 
 <p>
-	TEXT HERE.
+	This will be a simple contact form for the user to contact me with new ideas or concerns.
+	This section will also contain below it a link to a Twitter, Main Discord, and maybe my GitHub.
 </p>
 
 <br>
 
 <h1>Wireframes:</h1>
 
-<img src=""alt="">
+<!--<img src=""alt="">-->
 
 </body>
 </html>
